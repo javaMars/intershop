c9d3e12c15680257199642a37cf245eda42dd594
Alena <bedareva_a@inbox.ru>
2025-10-27 00:32:38 +0700

    Тесты для заказов

---
+++ src/test/java/ru/yandex/practicum/mymarket/OrderControllerTest.java
@@ -0,0 +1,75 @@
+package ru.yandex.practicum.mymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.yandex.practicum.mymarket.dto.OrderDto;
+import ru.yandex.practicum.mymarket.service.OrderService;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OrderService orderService;
+
+    @Test
+    void testViewAllOrders() throws Exception {
+        List<OrderDto> orders = Arrays.asList(
+                new OrderDto(1L, null, 100L),
+                new OrderDto(2L, null, 200L)
+        );
+        when(orderService.findAllOrders()).thenReturn(orders);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/orders"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("orders"))
+                .andExpect(model().attributeExists("orders"))
+                .andExpect(model().attribute("orders", orders));
+
+
+        verify(orderService, times(1)).findAllOrders();
+    }
+
+    @Test
+    void testViewOrder_ExistingOrder() throws Exception {
+        OrderDto orderDto = new OrderDto(1L, null, 100L);
+        when(orderService.findOrder(1L)).thenReturn(Optional.of(orderDto));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/orders/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("order"))
+                .andExpect(model().attributeExists("order"))
+                .andExpect(model().attribute("order", orderDto));
+
+        verify(orderService, times(1)).findOrder(1L);
+    }
+
+    @Test
+    void testViewOrder_OrderNotFound() throws Exception {
+        when(orderService.findOrder(500L)).thenReturn(Optional.empty());
+
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/orders/500"))
+                .andExpect(status().isNotFound());
+
+        verify(orderService, times(1)).findOrder(500L);
+    }
+}
+
---
+++ src/test/java/ru/yandex/practicum/mymarket/OrderServiceIntegrationTest.java
@@ -0,0 +1,65 @@
+package ru.yandex.practicum.mymarket;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import ru.yandex.practicum.mymarket.dto.OrderDto;
+import ru.yandex.practicum.mymarket.model.Order;
+import ru.yandex.practicum.mymarket.repository.OrderRepository;
+import ru.yandex.practicum.mymarket.repository.ProductRepository;
+import ru.yandex.practicum.mymarket.service.OrderServiceImpl;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class OrderServiceIntegrationTest {
+    @Autowired
+    private OrderServiceImpl orderServiceImpl;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+
+    @BeforeEach
+    void setUp() {
+        orderRepository.deleteAll(); // Удаляет все записи
+        orderRepository.flush();
+    }
+
+    @Test
+    @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+    void testFindAllOrders_shouldReturnDtoList() {
+
+        Order order1 = new Order();
+        order1.setTotalSum(1000L);
+        orderRepository.save(order1);
+
+        Order order2 = new Order();
+        order2.setTotalSum(2000L);
+        orderRepository.save(order2);
+
+        List<OrderDto> result = orderServiceImpl.findAllOrders();
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+
+        // Проверка конвертации полей
+        assertEquals(1000L, result.get(0).getTotalSum());
+        assertEquals(2000L, result.get(1).getTotalSum());
+    }
+}
