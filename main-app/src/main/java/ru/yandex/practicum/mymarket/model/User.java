d47f24b678ac9faa6670f9d2fdfae44f0be9fd01
Alena <bedareva_a@inbox.ru>
2026-01-02 00:14:21 +0700

    Новая таблица для хранения данных пользователей

---
+++ main-app/src/main/java/ru/yandex/practicum/mymarket/model/User.java
@@ -0,0 +1,43 @@
+package ru.yandex.practicum.mymarket.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table(name = "users")
+public class User {
+    @Id
+    @Column("id")
+    private Long id;
+
+    @Column("login")
+    private String login;
+
+    @Column("password")
+    private String password;
+    private Role role = Role.USER;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
